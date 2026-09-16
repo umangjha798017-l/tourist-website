54dfc01c14329dab331db96ca4db3d4c11351e06
umangjha798017-l <umangjha798017@gmail.com>
2026-09-16 22:25:58 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,21 @@
+
+
+
+# def cal_fact(n):
+#     fact=1
+#     for i in range(1, n+1): 
+        
+#         fact *= i
+
+#     print(fact)
+
+# cal_fact(12)
+
+def converter(usd_val):
+
+    inr_val =usd_val * 83
+
+    print(usd_val,"USD=",inr_val,"INR")
+
+converter(100)
+
