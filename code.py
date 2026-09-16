653605b8869521392fb40de2349412898964b10a
umangjha798017-l <umangjha798017@gmail.com>
2026-09-16 22:31:46 +0530

    Update code.py
    program of conversion of usd into inr

--- code.py
+++ code.py
@@ -1,16 +1,3 @@
-
-
-
-# def cal_fact(n):
-#     fact=1
-#     for i in range(1, n+1): 
-        
-#         fact *= i
-
-#     print(fact)
-
-# cal_fact(12)
-
 def converter(usd_val):
 
     inr_val =usd_val * 83
@@ -18,4 +5,3 @@ def converter(usd_val):
     print(usd_val,"USD=",inr_val,"INR")
 
 converter(100)
-
